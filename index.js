dd4f64d6b583d28910b2e660c76a33ef8218e009
Felice Forby <feliceforby@gmail.com>
2019-02-20 12:33:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function loadIssue() {
   let template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  let result = template(issues);
 
-  for (let i = 0; i < issues.length; i++) {
-    let result = template(issues[i]);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  }
+  document.getElementsByTagName("main")[0].innerHTML += result;
 }
