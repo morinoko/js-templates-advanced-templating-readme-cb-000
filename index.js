88ff8527c51dedc08b056bd31f88edca241a7a1e
Felice Forby <feliceforby@gmail.com>
2019-02-20 12:29:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 function loadIssue() {
-  var issue = {
-    state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:13 UTC",
-    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
-  }
+  let template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
 
-  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  var result = template(issue);
-  document.getElementsByTagName("main")[0].innerHTML += result;
+  for (let i = 0; i < issues.length; i++) {
+    let result = template(issues[i]);
+    document.getElementsByTagName("main")[0].innerHTML += result;
+  }
 }
